d55f4301dd8599f6fb35515841540ea9efe11d00
Davi Holanda <daviht7@gmail.com>
2021-06-30 21:23:28 -0300

    commit home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+            <p>
+              Get access to all the publications <br />
+              <span> for $ 9.90 monthly</span>
+            </p>
+          </h1>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
